0b8458dc11dccf1bde3105476f023b89831788de
Raul Rohjans <rauljann@hotmail.com>
2024-04-21 22:39:04 +0100

    Removed old components

---
+++ components/Logo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    export type LogoProps = {
+        /**
+         * Custom class to be used on the logo image component
+         */
+        class?: string
+    }
+
+    const props = withDefaults(defineProps<LogoProps>(), {
+        class: ''
+    })
+
+    const colorMode = useColorMode()
+    const getLogo = computed(() => {
+        if(colorMode.value == 'dark') return 'logo-light'
+        else return 'logo'
+    })
+
+
+</script>
+
+<template>
+    <div :class="props.class">
+        <div class="bg-logo dark:bg-logo-dark bg-no-repeat w-full h-[10vh]"></div>
+    </div>
+</template>
