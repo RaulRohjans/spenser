3a3a3605dfc5afabe1de283398a606a4893573b9
Raul Rohjans <rauljann@hotmail.com>
2024-05-13 22:53:08 +0100

    Added a better sample graph that uses SVG renderer

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,43 @@
 <script setup lang="ts">
-    import { use } from 'echarts/core';
-    import { CanvasRenderer } from 'echarts/renderers';
-    import { PieChart } from 'echarts/charts';
+    import { use } from 'echarts/core'
+    import VChart, { THEME_KEY } from 'vue-echarts'
+
     import {
-        TitleComponent,
-        TooltipComponent,
-        LegendComponent,
-    } from 'echarts/components';
-    import VChart, { THEME_KEY } from 'vue-echarts';
-    import { ref, provide } from 'vue';
+        GridComponent,
+        type GridComponentOption,
+    } from 'echarts/components'
+    
+    import { LineChart, type LineSeriesOption } from 'echarts/charts'
+    import { UniversalTransition } from 'echarts/features'
+    import { SVGRenderer } from 'echarts/renderers'
 
     use([
-        CanvasRenderer,
-        PieChart,
-        TitleComponent,
-        TooltipComponent,
-        LegendComponent,
-    ]);
+        GridComponent,
+        LineChart,
+        SVGRenderer,
+        UniversalTransition
+    ])
+
+    type EChartsOption = echarts.ComposeOption<
+        GridComponentOption | LineSeriesOption
+    >
 
     provide(THEME_KEY, 'dark');
 
-    const option = ref({
-        title: {
-            text: 'Traffic Sources',
-            left: 'center',
+    const option: Ref<EChartsOption> = ref({
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         },
-        tooltip: {
-            trigger: 'item',
-            formatter: '{a} <br/>{b} : {c} ({d}%)',
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+        yAxis: {
+            type: 'value'
         },
         series: [
             {
-            name: 'Traffic Sources',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '60%'],
-            data: [
-                { value: 335, name: 'Direct' },
-                { value: 310, name: 'Email' },
-                { value: 234, name: 'Ad Networks' },
-                { value: 135, name: 'Video Ads' },
-                { value: 1548, name: 'Search Engines' },
-            ],
-            emphasis: {
-                itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)',
-                },
-            },
-            },
-        ],
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+            }
+        ]
     })
 </script>
 
@@ -79,7 +61,7 @@
             }">
     
             <div class="flex flex-row justify-start items-center py-2 px-4">
-                <v-chart class="chart" :option="option" autoresize />
+                <v-chart class="chart"  :option="option" autoresize />
             </div>
         </UCard>
 
