e27821a16deb8b21754a15498b40fb59a464f0be
Raul Rohjans <rauljann@hotmail.com>
2024-05-25 18:34:57 +0100

    Added welcome message on dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+    const { data: authData } = useAuth()
     
+    const getUsername = computed(() => {
+        return authData.value ? authData.value.username : ''
+    })
 </script>
 
 <template>
-    <div class="flex flex-col items-center gap-4">    
+    <div class="flex flex-col items-center gap-4">
+        <h2 class="text-3xl font-semibold mb-4">Welcome {{ getUsername }}!</h2>
+
         <div class="flex flex-col sm:flex-row justify-center gap-4 sm:gap-0 sm:justify-between items-center w-full">
             <ChartSpendingOverTime height="50vh" class="w-full sm:w-[65%]"/>
             <ChartExpenseByCategory height="50vh" class="w-full sm:w-[32%]"/>
