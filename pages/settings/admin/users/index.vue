096b36221270003f6910e4bbb6d3ab4e81666b68
Raul Rohjans <rauljann@hotmail.com>
2024-05-25 14:28:31 +0100

    Added user table to the admin settings panel

--- pages/settings/admin/users/index.vue
+++ pages/settings/admin/users/index.vue
@@ -1,8 +1,154 @@
 <script setup lang="ts">
+    import type { TableSort, FetchTableDataResult, TableRow } from '@/types/Table'
+    import type { NuxtError } from '#app'
+    import type { Selectable } from 'kysely'
+    import type { User } from 'kysely-codegen'
 
+    const { token } = useAuth()
+    const tableObj = {
+        label: 'Users',
+        actions: ['edit', 'delete'],
+        columns: [{
+                key: 'id',
+                label: '#',
+                sortable: true
+            }, {
+                key: 'username',
+                label: 'Username',
+                sortable: true
+            }, {
+                key: 'first_name',
+                label: 'First Name',
+                sortable: true
+            }, {
+                key: 'last_name',
+                label: 'Last Name',
+                sortable: true
+            }, {
+                key: 'email',
+                label: 'Email',
+                sortable: true
+            }, {
+                key: 'is_admin',
+                label: 'Administrator',
+                sortable: true
+            }, {
+                key: 'actions',
+                label: 'Actions',
+                sortable: false,
+                searchable: false
+            }
+        ],
+    }
+    
+    const page: Ref<number> = ref(1)
+    const pageCount: Ref<number> = ref(10)
+    const searchQuery: Ref<string> = ref('')
+    const searchColumn: Ref<string> = ref('username')
+    const sort: Ref<TableSort> = ref({ column: 'id', direction: 'asc' as const })
+    const userLoaderObj: Ref<Omit<Selectable<User>, 'password'> | null> = ref(null)
+    const isModalOpen: Ref<boolean> = ref(false)
+    const tableDataKey: Ref<number> = ref(0)
+
+    // Fetch Data
+    const { data: tableData, pending: loading } = await useLazyAsyncData<FetchTableDataResult>
+    ('users', () => ($fetch)('/api/users', {  
+            method: 'GET',
+            headers: buildRequestHeaders(token.value), 
+            query: {
+                q: searchQuery.value,
+                qColumn: searchColumn.value,
+                page: page.value,
+                limit: pageCount.value,
+                sort: sort.value.column,
+                order: sort.value.direction
+            }
+    }), {
+        default: () => {
+            return {
+                success: false,
+                data: {
+                    totalRecordCount: 0,
+                    rows: []
+                }
+            }
+        },
+        watch: [page, searchQuery, searchColumn, pageCount, sort, tableDataKey]
+    })
+
+    const editUser = function(row: TableRow) {
+        userLoaderObj.value = {
+            id: row.id,
+            username: row.username,
+            first_name: row.first_name,
+            last_name: row.last_name,
+            email: row.email,
+            avatar: row.avatar,
+            is_admin: row.is_admin
+        }
+
+        toggleModal()
+    }
+
+    const delUser = function(row: TableRow) {
+        $fetch(`/api/currencies/delete`, {
+            method: 'POST',
+            headers: buildRequestHeaders(token.value),
+            body: { id: row.id }
+        }).then((data) => {
+            if(!data.success) {
+                displayMessage('An error ocurred when removing the user.', 'error')
+                return
+            }
+
+            displayMessage('User deleted successfully!', 'success')
+            reloadTableData()
+        }).catch((e: NuxtError) => {
+            displayMessage(e.statusMessage, 'error')
+        })
+    }
+
+    const toggleModal = function() {
+        isModalOpen.value = !isModalOpen.value
+    }
+
+    const reloadTableData = function() {
+        tableDataKey.value++
+    }
 </script>
 
 <template>
+    <div class="flex flex-row h-full">
+        <Table 
+            v-bind="tableObj"
+            :rows="tableData?.data.rows"
+            :row-count="tableData?.data.totalRecordCount"
+            :loading="loading"
+            class="bg-none shadow-none w-full"
+            v-model:page="page" 
+            v-model:pageCount="pageCount" 
+            v-model:search="searchQuery"
+            v-model:searchColumn="searchColumn"
+            v-model:sort="sort"
+            @edit-action="editUser"
+            @delete-action="delUser">
 
-    
+            <template #is_admin-data="{ row }">
+                {{ row.is_admin == true ? 'Yes' : 'No' }}
+            </template>
+
+            <template #extra-section>
+                <div class="flex flex-row items-end justify-end w-full">
+                    <UButton 
+                        icon="i-heroicons-plus"
+                        color="primary"
+                        size="xs"
+                        @click="toggleModal">
+                        Create User
+                    </UButton>
+                </div>
+            </template>
+        </Table>
+    </div>
+
 </template>
---
+++ server/api/users/index.get.ts
@@ -0,0 +1,70 @@
+import { ensureAuth } from "@/utils/authFunctions"
+import { db, applySearchFilter } from '@/utils/dbEngine'
+import { OrderByDirectionExpression } from "kysely"
+import { TableRow } from "~/types/Table"
+
+export default defineEventHandler(async (event) => {
+    // Read body params
+    const {
+        q: search,
+        qColumn: searchColumn,
+        page,
+        limit,
+        sort,
+        order
+    } = getQuery(event)
+    
+    const user = ensureAuth(event)
+
+    // Check if user is admin
+    if(!user.is_admin)
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'The user does not have permisson to access this resource.'
+        })
+    
+    // Build query to fetch categories
+    const parsedLimit: number = parseInt(limit?.toString() || '') || 100
+    const parsedPage: number = parseInt(page?.toString() || '') || 1
+    const query = db.selectFrom('user')
+        .selectAll()
+
+        // Search Filter
+        .$if(true, qb => applySearchFilter(qb, search?.toString(), searchColumn?.toString() || 'user.username'))
+
+        // Pager
+        .$if(!!page, (qb) => qb.offset((parsedPage - 1) * parsedLimit))
+
+        // Limit
+        .$if(!!limit, (qb) => qb.limit(parsedLimit))
+
+        // Sort
+        .$if(!!sort, (qb) => qb.orderBy(db.dynamic.ref<string>(`${sort}`), (order || 'asc') as OrderByDirectionExpression))
+
+    // Get total record count
+    const totalRecordsRes = await db.selectFrom('user')
+        .select(({ fn }) => [
+            fn.countAll<number>().as('total')
+        ])
+        .$if(true, qb => applySearchFilter(qb, search?.toString(), searchColumn?.toString() || 'user.username'))
+        .executeTakeFirst()
+
+    // Get rows
+    let rowRes
+    try { rowRes = await query.execute() }
+    catch(e) { console.log((e as Error).message) }
+
+    if(!totalRecordsRes)
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Could not load total user count.'
+        })
+        
+    return {
+        success: true,
+        data: {
+            totalRecordCount: Number(totalRecordsRes.total),
+            rows: rowRes || [] as TableRow[]
+        }
+    }
+})
