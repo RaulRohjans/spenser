55c40610cdf949805381e35fefca6a4c88e15dcf
Raul Rohjans <rauljann@hotmail.com>
2024-05-27 16:09:11 +0100

    Fixed issues with the budgets query

--- server/api/budgets/index.get.ts
+++ server/api/budgets/index.get.ts
@@ -9,45 +9,65 @@ export default defineEventHandler(async (event) => {
     // Build query to fetch budgets
     const query = await db.selectFrom('budget')
         .leftJoin('category', 'category.id', 'budget.category')
-        .innerJoin('transaction', 'transaction.user', 'budget.user')
+        .leftJoin('transaction',(join) => join
+            .onRef('transaction.user', '=', 'budget.user')
+            .on((eb) => eb
+                //Sum transaction value per category if it exists
+                .or([
+                    eb.and([
+                        eb("budget.category", "is not", null),
+                        eb('budget.category', '=', eb.ref('transaction.category'))
+                    ]),
+                    eb("budget.category", "is", null),
+                ])
+            )
+
+            // Apply time period to the expenses
+            .on((eb) => eb
+                .case()
+                .when("budget.period", "=", "daily")
+                .then(
+                    sql<boolean>`extract(day from transaction.date) = extract(day from current_date)`,
+                )
+
+                .when("budget.period", "=", "monthly")
+                .then(
+                    sql<boolean>`extract(month from transaction.date) = extract(month from current_date)`,
+                )
+        
+                .when("budget.period", "=", "quarterly")
+                .then(
+                    sql<boolean>`transaction.date <= current_date and
+                        transaction.date >= (current_date - INTERVAL '3 months')`,
+                )
+        
+                .when("budget.period", "=", "quarterly")
+                .then(
+                    sql<boolean>`transaction.date <= current_date and
+                        transaction.date >= (current_date - INTERVAL '6 months')`,
+                )
+        
+                .when("budget.period", "=", "quarterly")
+                .then(
+                    sql<boolean>`extract(year from transaction.date) = extract(year from current_date)`,
+                )
+                
+                .else(false)
+                .end()        
+            )
+        )
 
         .selectAll('budget')
         .select(['category.name as category_name', 'category.icon as category_icon'])
+
+        // Only add negative transactions to the sum (expenses)
         .select(({fn}) => [
-            fn.sum(sql<number>`"transaction"."value" * -1`).as('expenses'),
-            sql<number>`"budget"."value" - sum("transaction"."value" * -1)`.as('budget_left')
+            fn.sum(sql<number>`case when "transaction"."value" < 0 then "transaction"."value" * -1 when "transaction"."value" >= 0 then 0 end`).as('expenses')
         ])
 
-        .where(({ eb, or, and }) => and([
-            // Only get negative values (expenses)
-            eb('transaction.value', '<', '0'),
-
-            //Sum transaction value per category if it exists
-            or([
-                and([
-                    eb("budget.category", "is not", null),
-                    eb('budget.category', '=', eb.ref('transaction.category'))
-                ]),
-                eb("budget.category", "is", null),
-            ])
-        ]))
-
-        /* TODO: Replace this with the proper syntax after discord thread is closed with answer */
-        // Apply time period to the expenses
-        .where(sql`case 
-            when budget.period = 'daily' then (extract(day from "transaction"."date") = extract(day from CURRENT_DATE))
-            when budget.period = 'monthly' then (extract(month from "transaction"."date") = extract(month from CURRENT_DATE))
-            when budget.period = 'quarterly' then ("transaction"."date" <= CURRENT_DATE and
-                "transaction"."date" >= (current_date - INTERVAL '3 months'))
-            when budget.period = 'semi-annual' then ("transaction"."date" <= CURRENT_DATE and
-                "transaction"."date" >= (current_date - INTERVAL '6 months'))
-            when budget.period = 'yearly' then (extract(year from "transaction"."date") = extract(year from CURRENT_DATE)) end`,
-        sql``, sql``)
-
-        // Validade user        
+        // Validade user, transaction is validated in the join itself
         .where(({eb, and, or}) => and([
             eb('budget.user', '=', user.id),
-            eb('transaction.user', '=', user.id),
             or([
                 and([
                   sql<boolean>`"budget"."category" is not null`,
@@ -62,7 +82,7 @@ export default defineEventHandler(async (event) => {
         .orderBy('budget.order asc')
         .execute()
         
-    console.log(query)
+        
     return {
         success: true,
         data: query as BudgetDataObject[]
