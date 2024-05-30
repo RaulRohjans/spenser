ad4f9a6657c2bf6c2d57af4f929379adfab53b2f
Raul Rohjans <rauljann@hotmail.com>
2024-05-30 23:58:37 +0100

    Improved data importer file upload layout

--- pages/transactions/llm-data-importer/index.vue
+++ pages/transactions/llm-data-importer/index.vue
@@ -19,7 +19,7 @@
     const onImportData = function(event: FormSubmitEvent<Schema>) {
         let formData: FormData | { transactionText: string }
         let url = '/api/llm-data-importer/'
-
+        
         if(filesRef.value && filesRef.value.files && filesRef.value.files.length > 0) {
             formData = new FormData()
 
@@ -56,6 +56,10 @@
 
             // Disaply success message
             displayMessage(`OK`, 'success')
+
+            // Clear input fields
+            state.file = undefined
+            state.transactionText = undefined
         }).catch((e: NuxtError) => {
             displayMessage(e.statusMessage, 'error')
         })
@@ -74,19 +78,7 @@
             <UForm :state="state" class="p-6 flex flex-row justify-center items-center w-full" @submit="onImportData">
                 <div class="flex flex-col justify-center items-center w-full sm:w-[70%] gap-2">
                     <UFormGroup class="w-full border border-gray-200 dark:border-gray-700 rounded p-4 sm:p-8 mb-4">
-                        <div class="flex items-center justify-center h-40 w-40">
-                            <label for="dropzone-file" class="flex flex-col items-center justify-center border-2 h-40 w-40 border-gray-300 border-dashed rounded-full cursor-pointer bg-gray-50  hover:bg-gray-100">
-                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                    <svg class="w-8 h-8 mb-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                                    </svg>
-
-                                    <p class="mb-2 text-sm text-gray-500">Click to upload</p>
-                                </div>
-
-                                <input ref="filesRef" id="dropzone-file" type="file" class="hidden" />
-                            </label>
-                        </div>
+                        <input type="file" ref="filesRef" class="w-full cursor-pointer focus:outline-none border-0 form-input rounded-md placeholder-gray-400 dark:placeholder-gray-500 file:mr-1.5 file:font-medium file:text-gray-500 dark:file:text-gray-400 file:bg-transparent file:border-0 file:p-0 file:outline-none text-sm px-2.5 py-1.5 shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400" />
         
                         <div class="flex items-center align-center text-center w-full flex-row my-6">
                             <div class="flex border-gray-200 dark:border-gray-800 w-full border-t border-solid"></div>
