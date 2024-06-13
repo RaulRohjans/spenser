c78553957960993bdad25a078402c3f95db86207
Raul Rohjans <77687494+RaulRohjans@users.noreply.github.com>
2024-06-13 21:32:05 +0100

    Added checks to make sure non admin users can't become admin (#13)

--- server/api/account/update.post.ts
+++ server/api/account/update.post.ts
@@ -46,7 +46,7 @@ export default defineEventHandler(async (event) => {
         .set('last_name', last_name)
         .set('username', username)
         .set('email', email)
-        .set('is_admin', is_admin == 'true' ? true : false)
+        .$if(user.is_admin, (qb) => qb.set('is_admin', is_admin == 'true' ? true : false))
         .where('user.id', '=', user.id)
         .executeTakeFirst()
 
