cb6df6cf8ed52526522d4d5ad06d33c761856b2a
Raul Rohjans <rauljann@hotmail.com>
2024-06-16 00:55:54 +0100

    Improved notifier component

--- components/SNotification.vue
+++ components/SNotification.vue
@@ -1,13 +1,82 @@
 <script setup lang="ts">
+    import type { EmitEventCallback } from '~/types/Data'
 
+    export interface SNotificationProps {
+        /*
+         * Title of the notification
+         */
+        title: string
+
+        /*
+         * Message of the notification
+         */
+        message: string
+
+        /*
+         * Notification display timeout
+         */
+        timeout?: number
+
+        /*
+         * Type of notification to be displayed
+         */
+        type?: 'info' | 'warning' | 'error' | 'success'
+
+        /*
+         * Emit event callbacks
+         */
+        emitCallbacks?: { [key: string]: EmitEventCallback }
+    }
+
+    const props = withDefaults(defineProps<SNotificationProps>(), {
+        timeout: 5000,
+        type: 'info'
+    })
+
+    const emit = defineEmits<{
+        (event: 'close'): void
+    }>()
+
+    const getColor = computed(() => {
+        switch(props.type) {
+            case 'info': return 'primary'
+            case 'error': return 'error'
+            case 'success': return 'green'
+            case 'warning': return 'yellow'
+        }
+    })
+
+    const getIcon = computed(() => {
+        const prefix = 'i-heroicons-'
+
+        switch(props.type) {
+            case 'info': return `${prefix}information-circle`
+            case 'error': return `${prefix}x-circle`
+            case 'success': return `${prefix}check-circle`
+            case 'warning': return `${prefix}exclamation-circle`
+        }
+    })
+    
+    const onClose = function () {
+        // Run callbacks if there are any
+        if(props.emitCallbacks && 'close' in props.emitCallbacks) 
+            props.emitCallbacks.close()
+
+        emit('close')
+    }
 </script>
 
 <template>
-    <UNotification
-        icon="i-heroicons-check-circle"
-        description="This is a notification."
-        :id="3"
-        :timeout="0"
-        title="Notification"
-    />
+    <div class="absolute top-4 right-4 w-[20%]">
+        <div class="sticky">
+            <UNotification
+                :title="props.title"
+                :description="props.message"
+                :icon="getIcon"
+                :color="getColor"
+                :timeout="timeout"
+                @close="onClose"
+            />
+        </div>
+    </div>
 </template>
--- utils/Notifier.ts
+++ utils/Notifier.ts
@@ -1,16 +1,51 @@
-import SNotification from '~/components/SNotification.vue'
 import { createApp as vueCreateApp } from 'vue'
+import SNotification from '~/components/SNotification.vue'
+import type { SNotificationProps } from '~/components/SNotification.vue'
+import type { EmitEventCallback } from '~/types/Data'
 
 export class Notifier {
     static displayMessage(
         message: string | undefined | null,
         type: 'info' | 'warning' | 'error' | 'success' = 'info'
-    ) {    
+    ) {
+        const { t: $t } = useI18n()
+
         // Create container and add to DOM
         const mountEl = document.createElement('div')
         document.body.appendChild(mountEl)
     
-        // Mount instace to div
-        vueCreateApp(SNotification).mount(mountEl)
+        // Build title
+        let title = ''
+        switch(type) {
+            case 'error':
+                title = $t('Error')
+                break
+            case 'warning':
+                title = $t('Warning')
+                break
+            case 'info':
+                title = $t('Info')
+                break
+            case 'success':
+                title = $t('Success')
+                break
+        }
+
+
+        const emitCallbacks: { [key: string]: EmitEventCallback } = {
+            close: () => {
+                console.log('Hell Yeah!')
+            }
+        }
+
+        const props: SNotificationProps = {
+            title,
+            message: message || '',
+            type,
+            emitCallbacks
+        }
+
+        // Create app vue instance & mount
+        vueCreateApp(SNotification, {...props}).mount(mountEl)
     }
 }
