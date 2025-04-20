d673d84bb7ea31963fcdb2bce7cc46fe2a0d6971
Raul Rohjans <rauljann@hotmail.com>
2025-04-20 14:46:07 +0100

    Removed unnecessary import

--- stores/settings.ts
+++ stores/settings.ts
@@ -1,7 +1,6 @@
 import type { Selectable } from 'kysely'
 import type { Currency } from 'kysely-codegen'
 import type { UserSettingsObject } from '~/types/Data'
-import { defineStore } from "pinia"
 
 export const useSettingsStore = defineStore('settingsStore', {
     state: () => ({
