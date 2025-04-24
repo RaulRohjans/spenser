47551eab3728846c8260eafd0291f0fd5984c9f5
Raul Rohjans <rauljann@hotmail.com>
2025-04-24 19:00:53 +0100

    Added other transaction form modes

---
+++ pages/transactions/index/create.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    const router = useRouter()
+    const { t: $t } = useI18n()
+
+    const isOpen = true
+
+    function handleOpenChange(state: boolean) {
+        if(!state) router.push('/transactions')
+    }
+</script>
+
+<template>
+    <UModal
+        v-model:open="isOpen"
+        :title="$t('Create Transaction')"
+        @update:open="handleOpenChange">
+        <template #body>
+            <ModalTransaction
+                mode="create"
+                @successful-submit="router.push('/transactions')" />
+        </template>
+    </UModal>
+</template>
---
+++ pages/transactions/index/duplicate/[id].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    const route = useRoute()
+    const router = useRouter()
+    const { t: $t } = useI18n()
+    const id: number = Number.parseInt(route.params.id.toString())
+
+    const isOpen = true
+
+    function handleOpenChange(state: boolean) {
+        if(!state) router.push('/transactions')
+    }
+</script>
+
+<template>
+    <UModal
+        v-model:open="isOpen"
+        :title="$t('Duplicate Transaction')"
+        @update:open="handleOpenChange">
+        <template #body>
+            <ModalTransaction
+                :id="id"
+                mode="duplicate"
+                @successful-submit="router.push('/transactions')" />
+        </template>
+    </UModal>
+</template>
