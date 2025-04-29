ad881bf50dd19147587eacfab06c067410e6c492
Raul Rohjans <rauljann@hotmail.com>
2025-04-29 15:26:27 +0100

    Implemented new routing system in categories

---
+++ pages/categories/index/create.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    const router = useRouter()
+    const { t: $t } = useI18n()
+
+    const isOpen = true
+
+    const emit = defineEmits<{
+        (event: 'successful-submit'): void
+    }>()
+
+    function handleOpenChange(state: boolean) {
+        if(!state) router.push('/categories')
+    }
+
+    function onSuccessfulSubmit() {
+        emit('successful-submit')
+        router.push('/categories')
+    }
+</script>
+
+<template>
+    <UModal
+        v-model:open="isOpen"
+        :title="$t('Create Category')"
+        @update:open="handleOpenChange">
+        <template #body>
+            <ModalTransaction
+                mode="create"
+                @successful-submit="onSuccessfulSubmit" />
+        </template>
+    </UModal>
+</template>
