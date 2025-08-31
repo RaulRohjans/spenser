09a76bee55e4615c71667bd26382f0e484d81951
Raul Rohjans <rauljann@hotmail.com>
2025-08-31 23:49:24 +0100

    Updated copyright date

--- app/components/SFooter.vue
+++ app/components/SFooter.vue
@@ -3,7 +3,7 @@
         <div class="w-full py-3">
             <hr class="my-3 border-gray-200 dark:border-gray-700 mx-3" />
             <span class="block text-xs text-gray-500 sm:text-center dark:text-gray-400"
-                >© 2024
+                >© 2025
                 <a href="https://rohjans.com/" class="hover:underline"
                     >Raul Rohjans</a
                 >. MIT License.</span>
