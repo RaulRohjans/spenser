e4374a8a0e3537b3d37f8ea06cb2a0acf80ef720
Raul Rohjans <rauljann@hotmail.com>
2025-08-31 19:28:29 +0100

    Added more side padding

--- app/components/Sidebar/SSidebar.vue
+++ app/components/Sidebar/SSidebar.vue
@@ -25,7 +25,7 @@
     <UDrawer v-model:open="isOpen" direction="right" :handle="false" :handleOnly="true" :dismissible="true" :overlay="true">
         <template #content>
             <div class="h-full flex flex-col" :class="props.widthClass">
-                <div class="flex items-center justify-between px-4 py-3 border-b border-gray-200 dark:border-gray-800">
+                <div class="flex items-center justify-between px-6 py-3 border-b border-gray-200 dark:border-gray-800">
                     <h3 class="text-lg font-semibold">{{ props.title }}</h3>
                     <UButton
                         icon="i-heroicons-x-mark"
@@ -34,11 +34,11 @@
                         @click="isOpen = false" />
                 </div>
 
-                <div class="flex-1 overflow-y-auto px-4 py-3">
+                <div class="flex-1 overflow-y-auto px-6 py-3">
                     <slot />
                 </div>
 
-                <div class="flex items-center justify-between gap-2 px-4 py-3 border-t border-gray-200 dark:border-gray-800">
+                <div class="flex items-center justify-between gap-2 px-6 py-3 border-t border-gray-200 dark:border-gray-800">
                     <UButton
                         icon="i-heroicons-arrow-uturn-left"
                         color="neutral"
