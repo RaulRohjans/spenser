a2de042b7d566ea100515a6d5668cb67a83d9ce9
Raul Rohjans <rauljann@hotmail.com>
2025-09-15 19:44:34 +0100

    Adjusted import with AI page design

--- app/pages/transactions/import-ai/index.vue
+++ app/pages/transactions/import-ai/index.vue
@@ -114,8 +114,8 @@
                         </div>
                     </div>
                 </template>
-                <div class="flex-1 flex items-center justify-center">
-                    <div class="flex flex-col justify-center items-center w-full gap-4 px-2 sm:px-4">
+                <div class="flex-1 flex items-start justify-center pt-8 sm:pt-12">
+                    <div class="flex flex-col justify-start items-center w-full gap-4 px-2 sm:px-4">
                         <span class="mb-4">
                             {{
                                 $t(
