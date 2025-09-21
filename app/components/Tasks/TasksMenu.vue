51c0d4bc35f306db1a969f78f3db17218c751571
Raul Rohjans <rauljann@hotmail.com>
2025-09-21 20:27:19 +0100

    Added more missing translations

--- app/components/Tasks/TasksMenu.vue
+++ app/components/Tasks/TasksMenu.vue
@@ -16,11 +16,11 @@
 
     const formattedElapsed = (ms: number): string => {
         const s = Math.floor(ms / 1000)
-        if (s < 60) return `${s} sec`
+        if (s < 60) return `${s} ${$t('sec')}`
         const m = Math.floor(s / 60)
-        if (m < 60) return `${m} min`
+        if (m < 60) return `${m} ${$t('min')}`
         const h = Math.floor(m / 60)
-        return `${h} h ${m % 60} min`
+        return `${h} ${$t('h')} ${m % 60} ${$t('min')}`
     }
 
     const goToResult = async (task: AsyncTaskSummary) => {
